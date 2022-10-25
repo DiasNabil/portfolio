849ae04a3367509593cd1fb619a59dbeba9cedf7
Don Rogue <donrogue@protonmail.com>
2022-10-25 13:43:37 +0200

    version 2.0

--- README.md
+++
@@ -1,5 +0,0 @@
-��#� �p�o�r�t�f�o�l�i�o�
-�
-�
-
-https://diasnabil.github.io/portfolio/
---
+++ index.js
@@ -0,0 +1,24 @@
+const navButton = document.querySelector('.nav-toggler')
+const anchor = document.querySelectorAll('.anchor')
+const nav = document.querySelector('nav')
+
+const cvScroll = document.querySelector('.cv')
+
+
+
+
+// eventlistener sur le button et les encres pour animation Toggler
+
+navButton.addEventListener('click', toggleNav)
+
+anchor[0].addEventListener('click', toggleNav)
+anchor[1].addEventListener('click', toggleNav)
+anchor[2].addEventListener('click', toggleNav)
+
+
+// fonction Toggle du boutton de navigation
+
+function toggleNav() {
+    navButton.classList.toggle('active')
+    nav.classList.toggle('active')
+}
