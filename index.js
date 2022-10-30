fa07054b1a7a849f36031030c10fd6adace20df9
Don Rogue <donrogue@protonmail.com>
2022-10-30 12:10:15 +0100

    maj contact

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const anchor = document.querySelectorAll('.anchor')
 const nav = document.querySelector('nav')
 
 
-// eventlistener sur le button et les encres pour animation Toggler
+//                                                  eventlistener sur le button et les encres pour animation Toggler
 
 navButton.addEventListener('click', toggleNav)
 
@@ -12,57 +12,59 @@ anchor[1].addEventListener('click', toggleNav)
 anchor[2].addEventListener('click', toggleNav)
 
 
-// fonction Toggle du boutton de navigation
+        // fonction Toggle du boutton de navigation
 
-function toggleNav() {
+        function toggleNav() {
     navButton.classList.toggle('active')
     nav.classList.toggle('active')
-}
+        }
 
-// CAROUSEL 
+
+        
+
+//                                                                                 CAROUSEL 
+
 
 var imgs = document.querySelectorAll('.img')
 var btns = document.querySelectorAll('.btn')
-
-
 let currentSlide = 1
 
 
-    // Navigation manuelle 
+        // Navigation manuelle 
 
-    var manualNav = (manual) => {
+        var manualNav = (manual) => {
 
-        imgs.forEach((img) => {
-            img.classList.remove('active')
+            imgs.forEach((img) => {
+                img.classList.remove('active')
 
-            btns.forEach((btn => {
-                btn.classList.remove('active')
-            }))
-            
-        })
+                btns.forEach((btn => {
+                    btn.classList.remove('active')
+                }))
+                
+            })
 
-        imgs[manual].classList.add('active')
-        btns[manual].classList.add('active')
-
-       
+            imgs[manual].classList.add('active')
+            btns[manual].classList.add('active')
 
         
-        imgs[manual].scrollIntoView({
-            behavior: "smooth",
-            block: "nearest",
-            inline: "nearest",
-
-        })
-    }
-
-    btns.forEach((btn, i) => {
-        btn.addEventListener('click', () => {
-            manualNav(i);
-         
-            currentSlide = i;
 
             
+            imgs[manual].scrollIntoView({
+                behavior: "smooth",
+                block: "nearest",
+                inline: "nearest",
 
-        })
-    });
+            })
+        }
+
+        btns.forEach((btn, i) => {
+            btn.addEventListener('click', () => {
+                manualNav(i);
+            
+                currentSlide = i;
+
+                
+
+            })
+        });
 
