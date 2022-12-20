7dc3406baf0d9eb74d3e7994913977da6f4115d4
Don Rogue <diasnabil@protonmail.com>
2022-12-20 10:31:44 +0100

    MAJ

--- index.js
+++ index.js
@@ -20,51 +20,8 @@ anchor[2].addEventListener('click', toggleNav)
         }
 
 
-        
+
 
 //                                                                                 CAROUSEL 
 
-
-var imgs = document.querySelectorAll('.img')
-var btns = document.querySelectorAll('.btn')
-let currentSlide = 1
-
-
-        // Navigation manuelle 
-
-        var manualNav = (manual) => {
-
-            imgs.forEach((img) => {
-                img.classList.remove('active')
-
-                btns.forEach((btn => {
-                    btn.classList.remove('active')
-                }))
-                
-            })
-
-            imgs[manual].classList.add('active')
-            btns[manual].classList.add('active')
-
-        
-
-            
-            imgs[manual].scrollIntoView({
-                behavior: "smooth",
-                block: "nearest",
-                inline: "nearest",
-
-            })
-        }
-
-        btns.forEach((btn, i) => {
-            btn.addEventListener('click', () => {
-                manualNav(i);
-            
-                currentSlide = i;
-
-                
-
-            })
-        });
 
